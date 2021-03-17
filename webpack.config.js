b7e104f206f6ba955d43733a06d0104dacdd9e0c
yujo <bedro27@gmail.com>
2021-03-17 17:04:41 +0900

    chore: webpack babel config 추가

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,16 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
