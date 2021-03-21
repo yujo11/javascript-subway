0af29fe0f6d094f1720935d0fd2496690672d49a
yujo <bedro27@gmail.com>
2021-03-21 23:08:08 +0900

    init

--- src/js/constants/index.js
+++
@@ -1,3 +0,0 @@
-export { SNACKBAR_MESSAGE } from './snackbarMessage.js';
-export { LOCAL_STORAGE_KEY } from './standard.js';
-export { LOGIN_REQUIRED_TEMPLATE } from './template.js';
--- src/js/index.js
+++
@@ -1,6 +0,0 @@
-import '../css/index.css';
-import App from './components/App.js';
-
-window.addEventListener('DOMContentLoaded', () => {
-  new App();
-});
